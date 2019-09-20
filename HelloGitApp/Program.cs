e123ad70a51add27b838a72baa58bb7982aa3c8c
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:21:27 +0200

    a legkisebb es a valtoztatasok

--- HelloGitApp/Program.cs
+++ HelloGitApp/Program.cs
@@ -12,24 +12,39 @@ namespace HelloGitApp {
             Console.WriteLine("My first git project!");
             Console.WriteLine("Press enter to EXIT");
             Console.ReadLine();
-            //1.feladat
+            //2.feladat
             Console.WriteLine("Adja meg hogy hány számból álljon egy tömb, ahova számokat kérünk majd be: ");
             int meretSzam = Convert.ToInt32(Console.ReadLine());
             int[] bekertSzamok = new int[meretSzam];
             int i = 0;
-            while(i<10) {
+            while(i< meretSzam) {
                 Console.WriteLine("Kérem ajdon meg egy számot: ");
                 bekertSzamok[i] = Convert.ToInt32(Console.ReadLine());
                 i++;
             }
-
-            int legnagyobb = 0;
-            while(i<bekertSzamok.Length) {
+            i = 0;
+            //1. feladat
+            int legnagyobb = bekertSzamok[0];
+            while(i< meretSzam) {
                 if (bekertSzamok[i] > legnagyobb) {
                     legnagyobb = bekertSzamok[i];
                 }
+                i++;
             }
             Console.WriteLine("A legnagyobb megadott szám: {0}", legnagyobb);
+            //3.feladat
+            i = 0;
+            int legkissebb = bekertSzamok[0];
+            while (i < meretSzam) {
+                if (bekertSzamok[i] < legkissebb) {
+                    legkissebb = bekertSzamok[i];
+                }
+                i++;
+            }
+            Console.WriteLine("A legkisebb megadott szám: {0}", legkissebb);
+
+
+
 
             Console.ReadLine();
         }
